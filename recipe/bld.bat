4992f2aa598782a32ba6a208f22f9f8456e95428
Eric Lundby <Eric.Lundby@gmail.com>
2026-06-12 10:56:53 -0600

    Use the actual distribution.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,23 +2,28 @@
 
 mkdir %LIBRARY_BIN%
 
-:: Windows ARM64 only exists on Windows 10+, where UCRT is always part of the OS.
-:: Copy from system to ensure packages can find it in the environment rather than searching PATH.
-if "%target_platform%" == "win-arm64" (
-    echo ARM64 target detected - copying system UCRT to prevent PATH issues during bootstrapping.
-    xcopy "%SystemRoot%\System32\ucrtbase.dll" "%PREFIX%\" || exit /b 1
-    xcopy "%SystemRoot%\System32\ucrtbase.dll" "%LIBRARY_BIN%\" || exit /b 1
-    exit /b 0
-)
 
 %BUILD_PREFIX%/Library/usr/lib/p7zip/7z.exe x 22621.1.220506-1250.ni_release_WindowsSDK.iso -aoa
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
+:: Despite the name, this is the universal CRT redistributable for x86, x64, and arm64
 msiexec /a "%SRC_DIR%\Installers\Universal CRT Redistributable-x86_en-us.msi" /qb TARGETDIR="%SRC_DIR%\tmp"
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-xcopy "tmp\Windows Kits\10\Redist\%PKG_VERSION%\ucrt\DLLs\x64\"* "%PREFIX%"
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+if "%target_platform%" == "win-arm64" (
+    xcopy "tmp\Windows Kits\10\Redist\%PKG_VERSION%\ucrt\DLLs\arm64\"* "%PREFIX%"
+    if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-xcopy "tmp\Windows Kits\10\Redist\%PKG_VERSION%\ucrt\DLLs\x64\"* "%LIBRARY_BIN%"
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+    xcopy "tmp\Windows Kits\10\Redist\%PKG_VERSION%\ucrt\DLLs\arm64\"* "%LIBRARY_BIN%"
+    if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+) else (
+
+    xcopy "tmp\Windows Kits\10\Redist\%PKG_VERSION%\ucrt\DLLs\x64\"* "%PREFIX%"
+    if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+    xcopy "tmp\Windows Kits\10\Redist\%PKG_VERSION%\ucrt\DLLs\x64\"* "%LIBRARY_BIN%"
+    if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+)
+
+
